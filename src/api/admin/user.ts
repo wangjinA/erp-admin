565801440b40f23f50ac00f5d2f4aeb11a21af94
汪锦 <12774811+lqtt-wj@user.noreply.gitee.com>
2024-08-23 21:04:29 +0800

    feat: 登陆方式

--- src/api/admin/user.ts
+++ src/api/admin/user.ts
@@ -1,56 +1,61 @@
-import { LoginResponse } from '@/types/user';
-import baseAxios,{ getRequestEndInfo }  from '..';
-import { APIListResponse, APIResponse } from '../type';
-import { EndType, getEndType } from '@/routes';
+import baseAxios, { getRequestEndInfo } from '..'
+import { APIListResponse, APIResponse } from '../type'
 
-export const login = (params) => {
-  const { userLoginAccount, userLoginPassword, captcha } = params;
+import { EndType, getEndType } from '@/routes'
+import { LoginResponse } from '@/types/user'
+
+export function login(params, sessionId) {
+  const { userLoginAccount, userLoginPassword, captcha } = params
   return baseAxios.post<APIResponse<LoginResponse>>('/api/system/login', {
     captcha,
     userLoginAccount,
     userLoginPassword,
-  });
-};
-export const loginExit = () => {
-  return baseAxios.post<APIResponse<LoginResponse>>('/api/system/exit');
-};
+  }, {
+    headers: {
+      sessionId,
+    },
+  })
+}
+export function loginExit() {
+  return baseAxios.post<APIResponse<LoginResponse>>('/api/system/exit')
+}
 
-export const getCaptcha = (randomVal?: any) => {
-  const val = randomVal ? `?${randomVal}` : '';
-  return getRequestEndInfo.baseUrl + '/api/code/captcha.jpg' + val; //! todo 确定一下
-};
+export function getCaptcha(randomVal?: any) {
+  const val = randomVal ? `?sessionId=${randomVal}` : ''
+  return `${getRequestEndInfo.baseUrl}/api/code/captcha.jpg${val}`
+}
 
 export interface UserInfo {
-  createBy: number;
-  createTime: string;
-  deleteStatus: number;
-  deptId: number;
-  deptName: string;
-  headImg: string;
-  id: number;
-  isAdmin: number;
-  remark: string;
-  roleIds: number[];
-  roleName: string[];
-  telephone: string;
-  tenantryId: number;
-  updateBy: number;
-  updateTime: string;
-  userLoginAccount: string;
-  userName: string;
-  userPassword: string;
-  userSex: number;
-  userStatus: number;
+  createBy: number
+  createTime: string
+  deleteStatus: number
+  deptId: number
+  deptName: string
+  headImg: string
+  id: number
+  isAdmin: number
+  remark: string
+  roleIds: number[]
+  roleName: string[]
+  telephone: string
+  tenantryId: number
+  updateBy: number
+  updateTime: string
+  userLoginAccount: string
+  userName: string
+  userPassword: string
+  userSex: number
+  userStatus: number
 }
 
 export const userAPI = {
   personalCenter() {
-    if(getEndType() === EndType.CLIENT){
-      return baseAxios.get<APIListResponse<UserInfo>>('/api/tenantry/user/personalCenter');
+    if (getEndType() === EndType.CLIENT) {
+      return baseAxios.get<APIListResponse<UserInfo>>('/api/tenantry/user/personalCenter')
     }
-    return baseAxios.get<APIListResponse<UserInfo>>('/api/user/personalCenter');
+    return baseAxios.get<APIListResponse<UserInfo>>('/api/user/personalCenter')
   },
   updateSelf(body: Partial<UserInfo>) {
-    return baseAxios.post<APIResponse>('/api/user/updateSelf', body);
+    return baseAxios.post<APIResponse>('/api/user/updateSelf', body)
   },
-};
+}
--- src/components/Login/form.tsx
+++ src/components/Login/form.tsx
@@ -1,44 +1,45 @@
 import {
-  Form,
-  Input,
-  Checkbox,
-  Link,
   Button,
-  Space,
-  Message,
+  Form,
   Image,
+  Input,
+  Message,
+  Space,
   Spin,
-} from '@arco-design/web-react';
-import { FormInstance } from '@arco-design/web-react/es/Form';
-import { IconLock, IconSafe, IconUser } from '@arco-design/web-react/icon';
-import React, { useEffect, useRef, useState } from 'react';
-import useStorage from '@/utils/useStorage';
-import useLocale from '@/utils/useLocale';
-import locale from './locale';
-import styles from './style/index.module.less';
-import { useLocalStorageState, useRequest } from 'ahooks';
-import { getCaptcha, login } from '@/api/admin/user';
-import { random } from 'lodash';
-import { getRequestEndInfo, SuccessCode } from '@/api';
+} from '@arco-design/web-react'
+import { FormInstance } from '@arco-design/web-react/es/Form'
+import { IconLock, IconSafe, IconUser } from '@arco-design/web-react/icon'
+
+import { useLocalStorageState, useRequest } from 'ahooks'
+
+import { random } from 'lodash'
+import React, { useEffect, useRef, useState } from 'react'
+
+import locale from './locale'
+import styles from './style/index.module.less'
+
+import { SuccessCode, getRequestEndInfo } from '@/api'
+import { getCaptcha, login } from '@/api/admin/user'
 import {
   EndType,
   EndTypeTextMap,
+  LoginPathMap,
   getEndType,
   getEndTypeName,
-  LoginPathMap,
-  toLoginPage,
-} from '@/routes';
+} from '@/routes'
+import useLocale from '@/utils/useLocale'
+import useStorage from '@/utils/useStorage'
 
 export default function LoginForm() {
-  const formRef = useRef<FormInstance>();
-  const [errorMessage, setErrorMessage] = useState('');
-  const [loginParams, setLoginParams, removeLoginParams] =
-    useStorage('loginParams');
-  const [value, setValue] = useLocalStorageState('userInfo');
-  const [randomStr, setRandomStr] = useState(null);
-  const t = useLocale(locale);
+  const formRef = useRef<FormInstance>()
+  const [errorMessage, setErrorMessage] = useState('')
+  const [loginParams, setLoginParams, removeLoginParams]
+    = useStorage('loginParams')
+  const [value, setValue] = useLocalStorageState('userInfo')
+  const [randomStr, setRandomStr] = useState(null)
+  const t = useLocale(locale)
 
-  const [rememberPassword, setRememberPassword] = useState(!!loginParams);
+  const [rememberPassword, setRememberPassword] = useState(!!loginParams)
 
   const {
     run: loginHandler,
@@ -46,35 +47,37 @@ export default function LoginForm() {
     data,
   } = useRequest(
     async (params) => {
-      const res = await login(params);
+      const res = await login(params, randomStr)
       if (res.data.code === SuccessCode) {
-        setValue(res.data.data);
-        localStorage.setItem(getRequestEndInfo.tokenKey, res.data.data.token);
-        localStorage.removeItem('scan-entrepot');
-        afterLoginSuccess(params);
-      } else {
-        Message.error(res.data.msg || t['login.form.login.errMsg']);
+        setValue(res.data.data)
+        localStorage.setItem(getRequestEndInfo.tokenKey, res.data.data.token)
+        localStorage.removeItem('scan-entrepot')
+        afterLoginSuccess(params)
+      }
+      else {
+        Message.error(res.data.msg || t['login.form.login.errMsg'])
       }
     },
     {
       manual: true,
-    }
-  );
+    },
+  )
   useEffect(() => {
-    localStorage.removeItem(getRequestEndInfo.tokenKey);
-  }, []);
+    localStorage.removeItem(getRequestEndInfo.tokenKey)
+  }, [])
 
   function afterLoginSuccess(params) {
     // 记住密码
     if (rememberPassword) {
-      setLoginParams(JSON.stringify(params));
-    } else {
-      removeLoginParams();
+      setLoginParams(JSON.stringify(params))
+    }
+    else {
+      removeLoginParams()
     }
     // 记录登录状态
-    localStorage.setItem('userStatus', 'login');
+    localStorage.setItem('userStatus', 'login')
     // 跳转首页
-    window.location.href = '/' + getEndType();
+    window.location.href = `/${getEndType()}`
   }
 
   // function loginHandler(params) {
@@ -97,19 +100,19 @@ export default function LoginForm() {
 
   function onSubmitClick() {
     formRef.current.validate().then((values) => {
-      loginHandler(values);
-    });
+      loginHandler(values)
+    })
   }
 
   // 读取 localStorage，设置初始值
   useEffect(() => {
-    const rememberPassword = !!loginParams;
-    setRememberPassword(rememberPassword);
+    const rememberPassword = !!loginParams
+    setRememberPassword(rememberPassword)
     if (formRef.current && rememberPassword) {
-      const parseParams = JSON.parse(loginParams);
-      formRef.current.setFieldsValue(parseParams);
+      const parseParams = JSON.parse(loginParams)
+      formRef.current.setFieldsValue(parseParams)
     }
-  }, [loginParams]);
+  }, [loginParams])
 
   return (
     <div className={styles['login-form-wrapper']}>
@@ -168,12 +171,13 @@ export default function LoginForm() {
               />
             </Form.Item>
             <Image
-              width={220}
-              loader={
+              width={135}
+              className="flex-shrink-0 cursor-pointer"
+              loader={(
                 <div className="flex justify-center">
                   <Spin size={4} dot></Spin>
                 </div>
-              }
+              )}
               preview={false}
               src={getCaptcha(randomStr)}
               onClick={() => setRandomStr(random(1, 99999))}
@@ -196,9 +200,10 @@ export default function LoginForm() {
             className={styles['login-form-register-btn']}
             onClick={() => {
               if (getEndType() === EndType.ADMIN) {
-                window.location.href = LoginPathMap.client;
-              } else {
-                window.location.href = LoginPathMap.admin;
+                window.location.href = LoginPathMap.client
+              }
+              else {
+                window.location.href = LoginPathMap.admin
               }
             }}
           >
@@ -211,5 +216,5 @@ export default function LoginForm() {
         </Space>
       </Form>
     </div>
-  );
+  )
 }
