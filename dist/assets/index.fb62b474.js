35f083ed4f26595d6f2ba09b17869d5c0f4a6c3e
汪锦 <12774811+lqtt-wj@user.noreply.gitee.com>
2024-12-19 23:07:50 +0800

    1

--- dist/assets/index.fb62b474.js
+++
@@ -1,1 +0,0 @@
-import{j as a,ay as s}from"./vendor.ad15203e.js";/* empty css              */var i=({value:r})=>a(s,{href:`https://www.baidu.com/s?wd=${r}`,target:"_blank",children:r});export{i as T};
