7fbebfb82175e70150ce7541c638c389285ff37b
汪锦 <12774811+lqtt-wj@user.noreply.gitee.com>
2024-12-20 16:00:30 +0800

    1

--- dist/assets/index.e19d3493.js
+++
@@ -1,1 +0,0 @@
-import{j as a,ay as s}from"./vendor.71e20600.js";/* empty css              */var i=({value:r})=>a(s,{href:`https://www.baidu.com/s?wd=${r}`,target:"_blank",children:r});export{i as T};
