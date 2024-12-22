a01a2b429b0104d4588200f739ea2f833855008c
汪锦 <12774811+lqtt-wj@user.noreply.gitee.com>
2024-12-22 20:34:45 +0800

    1

---
+++ dist/assets/shopStore.2c879861.js
@@ -0,0 +1,1 @@
+import{b as e}from"./index.284063d0.js";const s={getList(t){return e.post("/api/tenantry/store/list",t)},getAuthLink(){return e.post("/api/tenantry/store/create/accredit/url")},unbind(t){return e.get(`/api/tenantry/store/remove/${t}`)}};export{s};
