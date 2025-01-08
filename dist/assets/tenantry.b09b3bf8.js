6c36e2031d52f4a0bfa451b4d0e69cfb71c009bc
汪锦 <12774811+lqtt-wj@user.noreply.gitee.com>
2025-01-08 10:57:24 +0800

    1

--- dist/assets/tenantry.b09b3bf8.js
+++
@@ -1,1 +0,0 @@
-import{b as e}from"./index.284063d0.js";const n={getList(t){return e.post("/api/tenantry/list",t)},getDPList(t){return e.post("/api/tenantry/list/user",t)},get(t){return e.get("/api/tenantry/info/${id}")},create(t){return e.post("/api/tenantry/insert",t)},update(t){return e.post("/api/tenantry/update",t)},remove(t){return e.get(`/api/tenantry/remove/${t}`)}};export{n as t};
