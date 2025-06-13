2d0830f8900e397812158be1e2c0cb41d3e7d677
Wow Jin - MacHome <1161520645@qq.com>
2025-06-14 02:58:53 +0800

    1

--- src/constants/order.ts
+++ src/constants/order.ts
@@ -1,2 +1,19 @@
 // 只对这些物流商进行批量申请面单
 export const batchApplyShippingCarrierList = ['蝦皮店到店', '7-ELEVEN', 'OK Mart', '全家', '萊爾富']
+
+export const ShippingCarrierColorMap = {
+  '线下7-ELEVEN': 'red',
+  "线下新竹物流": 'orangered',
+  "线下全家": 'orange',
+  "OK Mart": 'gold',
+  "萊爾富": 'lime',
+  "蝦皮店到店": 'green',
+  "賣家宅配": 'blue',
+  "全家": 'arcoblue',
+  "7-ELEVEN": 'purple',
+  "宅配通": 'pinkpurple',
+  "黑貓宅急便": 'magenta',
+  "新竹物流": '#f53f3f',
+  "嘉里快遞": '#7816ff',
+  "店到家宅配": '#00b42a',
+}
